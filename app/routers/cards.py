acf10431a01950010b0910aba7e4080fb599eb52
birrquick-oss <birrquick@gmail.com>
2026-09-16 01:34:57 +0300

    Update cards.py

--- app/routers/cards.py
+++ app/routers/cards.py
@@ -41,13 +41,13 @@ def get_target_bot_card_count() -> int:
     hour = now.hour
 
     if 6 <= hour < 13:
-        return random.randint(100, 200)
+        return random.randint(50, 100)
     elif 13 <= hour <= 23:
-        return random.randint(200, 300)
-    elif 0 <= hour < 3:
-        return random.randint(200, 300)
-    else:
         return random.randint(100, 200)
+    elif 0 <= hour < 3:
+        return random.randint(100, 200)
+    else:
+        return random.randint(50, 100)
 
 async def trigger_bot_card_purchases(game_id: int, bet_amount: float = 10.0):
     """
