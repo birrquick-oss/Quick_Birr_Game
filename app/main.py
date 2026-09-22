0f05db541c8b076ae0b04346ba8d1189e6be4ad9
birrquick-oss <birrquick@gmail.com>
2026-09-22 04:02:56 +0300

    Add Plinko router to main application

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ from app.routers.games import router as games_router
 from app.routers.cards import router as cards_router
 from app.routers.users import router as users_router
 from app.routers.slots import router as slots_router
+from app.routers.plinko import router as plinko_router
 from app.websocket import router as websocket_router, manager
 from app.game_engine import engine
 
@@ -94,6 +95,7 @@ app.include_router(games_router)
 app.include_router(cards_router)
 app.include_router(users_router)
 app.include_router(slots_router)
+app.include_router(plinko_router)
 app.include_router(websocket_router)
 
 
