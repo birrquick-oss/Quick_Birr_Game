99473724f88a918d3ae3138cc8e323b7b3840e84
birrquick-oss <birrquick@gmail.com>
2026-09-26 02:19:43 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from app.database import SessionLocal, initialize_database
 from app.models import User
 from app.seed_cards import seed_cards  # 👈 Seed cards import ተደርጓል
 
-# Routerዎች
+# Routerዎች (ነባሮቹ)
 from app.routers.games import router as games_router
 from app.routers.cards import router as cards_router
 from app.routers.users import router as users_router
@@ -21,6 +21,13 @@ from app.routers.plinko import router as plinko_router
 from app.routers.roulette import router as roulette_router
 from app.routers.blackjack import router as blackjack_router
 from app.routers.mines import router as mines_router
+
+# 🚀 Routerዎች (አዳዲሶቹ 4 ጨዋታዎች)
+from app.routers.crash import router as crash_router
+from app.routers.keno import router as keno_router
+from app.routers.fishing import router as fishing_router
+from app.routers.chicken import router as chicken_router
+
 from app.websocket import router as websocket_router, manager
 from app.game_engine import engine
 
@@ -102,6 +109,13 @@ app.include_router(plinko_router)
 app.include_router(roulette_router)
 app.include_router(blackjack_router)
 app.include_router(mines_router)
+
+# 🚀 አዳዲሶቹ 4 የጨዋታ Routerዎች
+app.include_router(crash_router)
+app.include_router(keno_router)
+app.include_router(fishing_router)
+app.include_router(chicken_router)
+
 app.include_router(websocket_router)
 
 
